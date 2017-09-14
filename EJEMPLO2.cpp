17be27764bc77b5ec84967c1729771b6afd1f7ed
Michelvt23 <31973581+Michelvt23@users.noreply.github.com>
2017-09-14 17:56:13 -0500

    Add files via upload

---
+++ EJEMPLO2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main()
+  {
+  std::cout << "Bienvenido ";
+  std::cout << "a C++!\n";
+ 
+  return 0; // indica que el programa terminó correctamente
+ 
+  } // fin de la función main
